6198a8df848e9a52d3bb77ef0f67c6273d7fe52b
agutan <alex.gutan.m@gmail.com>
2020-10-22 14:19:41 +0300

    improv

---
+++ src/components/Counter.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <h1>Counter: {{ counter }} <span v-if="isBelowZero()">Gone below zero</span></h1>
+        <button @click="increment">Increase</button>
+        <button @click="decrease">Decrease</button>
+        <input type="number" v-model="counter">
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'Counter',
+        data() {
+            return {
+                counter: 10,
+            }
+        },
+        methods: {
+            increment() {
+                this.counter = Number.parseInt(this.counter) + 1
+            },
+            decrease() {
+                this.counter = this.counter - 1
+            },
+            isBelowZero() {
+                return Number.parseInt(this.counter) < 0
+            }
+        }
+    }
+</script>
---
+++ src/components/Posts.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <h1>Posts</h1>
+        <ul>
+            <li v-for="post in posts" :key="post.id">
+                UserID {{ post.userId }} <br>
+                ID {{ post.id }} <br>
+                Title {{ post.title }} <br>
+                Body {{ post.body }}
+                <hr>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'Posts',
+        data() {
+            return {
+                posts: []
+            }
+        },
+        async created() {
+            const postsResponse = await fetch("https://jsonplaceholder.typicode.com/posts");
+            const postsJson = await postsResponse.json();
+            this.posts = postsJson
+        }
+    }
+</script>
